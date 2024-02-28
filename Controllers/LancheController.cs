412dd2797b22daeba6dc46e5f8bb6148715d6545
Mattheus Peixoto <mattheuspeixoto@gmail.com>
2024-02-28 00:35:17 -0300

    Implementar o Controller e a View para Lanches e melhorar a ortografia no Modelo

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers {
+    public class LancheController : Controller {
+
+        private readonly iLanchesRespotory _lancheRepository;
+
+        public LancheController(iLanchesRespotory lancheRepository) {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List() {
+            var lanche = _lancheRepository.Lanches;
+            return View(lanche);
+        }
+    }
+}
--- Models/Lanche.cs
+++ Models/Lanche.cs
@@ -9,34 +9,34 @@ namespace LanchesMac.Models
         [Key]
         public int LancheId { get; set; }
 
-        [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+        [Required(ErrorMessage = "O Nome do Lanche deve ser informado")]
         [Display(Name = "Nome do Lanche")]
-        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no Máximo {2} Caracteres")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
         [Display(Name = "Descrição do Lanche")]
         [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
-        [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição deve ter no máximo {1} caracteres")]
         public string DescricaoCurta { get; set; }
 
-        [Required(ErrorMessage = "O descrição detalhada do lanche deve ser informada")]
+        [Required(ErrorMessage = "A descrição detalhada do lanche deve ser informada")]
         [Display(Name = "Descrição detalhada do Lanche")]
         [MinLength(20, ErrorMessage = "Descrição detalhada deve ter no mínimo {1} caracteres")]
-        [MaxLength(200, ErrorMessage = "Descrição detalhada pode exceder {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição detalhada deve ter no maximo {1} caracteres")]
         public string DescricaoDetalhada { get; set; }
 
-        [Required(ErrorMessage = "Informe o preço do lanche")]
-        [Display(Name = "Preço")]
+        [Required(ErrorMessage = "Informe o Preço do Lanche")]
+        [Display(Name = "Preço do Lanche")]
         [Column(TypeName ="decimal(10,2)")]
         [Range(1,999.99,ErrorMessage ="O preço deve estar entre 1 e 999,99")]
         public decimal Preco { get; set; }
 
-        [Display(Name = "Caminho Imagem Normal")]
+        [Display(Name = "Caminho da Imagem Normal")]
         [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemUrl { get; set; }
 
-        [Display(Name = "Caminho Imagem Miniatura")]
+        [Display(Name = "Caminho da Imagem Miniatura")]
         [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemThumbnailUrl { get; set; }
 
