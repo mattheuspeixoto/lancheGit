9b97219ed0c5bcede643503de7bb3a8de8c59251
Mattheus Peixoto <mattheuspeixoto@gmail.com>
2024-03-06 22:03:35 -0300

    Criando uma TagHelper (Aula 56)

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers;
+
+public class ContatoController : Controller{
+    public IActionResult Index(){
+        return View();
+    }
+}
