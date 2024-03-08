c0a393a9c1c9c186ff1759966cbc074c8cb9b385
Mattheus Peixoto <mattheuspeixoto@gmail.com>
2024-03-07 21:57:36 -0300

    Adicionar as Classes (Pedido e PedidoDetalhes) e criar as tabelas correspondentes no Banco de Dados

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -10,7 +10,9 @@ namespace LanchesMac.Context
         }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
-         public DbSet<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
 
     }
 }
