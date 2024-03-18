8187d49a739d5ef45c304ad6c272cb818a9ff0e7
Mattheus Peixoto <mattheuspeixoto@gmail.com>
2024-03-17 23:27:59 -0300

    Adicionar TagHelper do Telefone

---
+++ TagHelper/TelefoneTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace LanchesMac.TagHelpers;
+
+public class TelefoneTagHelper : TagHelper {
+    public string Endereco { get; set; }
+    public string Conteudo { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output) {
+        output.TagName = "a";
+        output.Attributes.SetAttribute("href", "tel:+55" + Endereco);
+        output.Content.SetContent(Conteudo);
+    }
+}
+
