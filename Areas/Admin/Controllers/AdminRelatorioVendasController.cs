f40a7a59aba5155b3a9f13c3aac73718e4fad29c
Mattheus Peixoto <mattheuspeixoto@gmail.com>
2024-03-21 21:48:06 -0300

    Ajustar Layout Relatorio De Vendas

--- Areas/Admin/Controllers/AdminRelatorioVendasController.cs
+++ Areas/Admin/Controllers/AdminRelatorioVendasController.cs
@@ -1,16 +1,20 @@
 ﻿using LanchesMac.Areas.Admin.Servicos;
+using LanchesMac.Context;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LanchesMac.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminRelatorioVendasController : Controller
     {
+        private readonly AppDbContext _context;
         private readonly RelatorioVendasService relatorioVendasService;
 
-        public AdminRelatorioVendasController(RelatorioVendasService _relatorioVendasService)
+        public AdminRelatorioVendasController(RelatorioVendasService _relatorioVendasService, AppDbContext context)
         {
             relatorioVendasService = _relatorioVendasService;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -18,23 +22,29 @@ namespace LanchesMac.Areas.Admin.Controllers
             return View();
         }
 
-        public async Task<IActionResult> RelatorioVendasSimples(DateTime? minDate,
-            DateTime? maxDate)
+        public async Task<IActionResult> RelatorioVendasSimples(DateTime? minDate, DateTime? maxDate, int pageindex = 1, string sort = "PedidoId")
         {
+            //var resultado = from obj in _context.Pedidos select obj;
+            string filter = "";
+
             if (!minDate.HasValue)
             {
-                minDate = new DateTime(DateTime.Now.Year,1, 1);
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
             }
             if (!maxDate.HasValue)
             {
                 maxDate = DateTime.Now;
             }
+            var resultado = _context.Pedidos
+                            .Include(l => l.PedidoItens)
+                            .ThenInclude(l => l.Lanche)
+                            .Where(p => p.PedidoEnviado >= minDate && p.PedidoEnviado <= maxDate)
+                            .OrderByDescending(a => a.PedidoId)
+                            .AsNoTracking().ToList();
 
-            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
-            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
-
-            var result = await relatorioVendasService.FindByDateAsync(minDate, maxDate);
-            return View(result);
+            //var model = await PagingList.CreateAsync(resultado, 10, pageindex, "-PedidoId","PedidoId");
+            // model.RouteValue = new RouteValueDictionary { { "filter", filter } };
+            return View(resultado);
         }
     }
 }
